896f0b64e4bf2516d4aa6231e78cd68fae44ea8a
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-01-21 20:45:46 +0530

    Add sound

--- script.js
+++ script.js
@@ -10,26 +10,23 @@ function typeWriter(el) {
   setInterval(() => typeWriter(el), 8000);
 }
 
-typeWriter(elementEl); // function typeWriter(el) {
-//   const textArray = el.innerHTML.split("");
-//   el.innerHTML = "";
-//   textArray.forEach((letter, i) =>
-//     setTimeout(() => (el.innerHTML += letter), 95 * i)
-//   );
-//   setInterval(() => typeWriter(el), 10000);
-// }
-
-// typeWriter(tagline);
+typeWriter(elementEl);
 
 document.addEventListener("click", (e) => {
   let body = document.querySelector("body");
   let div = document.createElement("div");
+  let x = document.getElementById("myAudio");
   let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   div.style.left = `${e.clientX - 10}px`;
   div.style.top = `${e.clientY - 10}px`;
   div.style.backgroundColor = randomColor;
   div.setAttribute("class", "dot");
   body.append(div);
+  function playAudio() {
+    x.play();
+  }
+  playAudio();
+
   setTimeout(() => {
     body.removeChild(div);
   }, 1100);
