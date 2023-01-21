16aeafecebec4d0fbf79de08153b643a804b1450
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-01-21 20:09:41 +0530

    Make responsive

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+"use strict";
+
 function typeWriter(el) {
   const textArray = el.innerHTML.split("");
   el.innerHTML = "";
   textArray.forEach((letter, i) =>
     setTimeout(() => (el.innerHTML += letter), 95 * i)
   );
-  setInterval(() => typeWriter(el), 10000);
+
+  setInterval(() => typeWriter(el), 8000);
 }
 
-typeWriter(tagline);
+typeWriter(elementEl); // function typeWriter(el) {
+//   const textArray = el.innerHTML.split("");
+//   el.innerHTML = "";
+//   textArray.forEach((letter, i) =>
+//     setTimeout(() => (el.innerHTML += letter), 95 * i)
+//   );
+//   setInterval(() => typeWriter(el), 10000);
+// }
+
+// typeWriter(tagline);
 
 document.addEventListener("click", (e) => {
   let body = document.querySelector("body");
